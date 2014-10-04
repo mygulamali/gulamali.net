114b610479652159981fb422a13128e9953e6382
Murtaza Gulamali <mygulamali@gmail.com>
2014-10-04 21:44:24 +0100

    Edit conditional and speed of sliding abstract.

--- source/assets/javascripts/_publications.js
+++ source/assets/javascripts/_publications.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $('.abstract-toggle').on('click', function (e) {
-    $(this).parent().find('.abstract-content').slideToggle('slow');
+    $(this).parent().find('.abstract-content').slideToggle('fast');
     $(this).find('i').toggleClass('icon-chevron-right icon-chevron-down');
     var action = $(this).find('span');
-    action.text() == 'Show' ? action.text('Hide') : action.text('Show');
+    action.text() === 'Show' ? action.text('Hide') : action.text('Show');
   });
 });
