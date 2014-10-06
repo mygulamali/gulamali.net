e1b7e385ec790649186074c54c539f677191b695
Murtaza Gulamali <mygulamali@gmail.com>
2014-10-06 17:17:26 +0100

    Make footer stick to the bottom of the window if view is too tall.

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,2 +1,8 @@
 //= require jquery
 //= require_tree .
+
+$(document).ready(function () {
+  if ($('body').height() < $(window).height()) {
+    $('footer').css('position', 'absolute').css('bottom', '0');
+  }
+});
