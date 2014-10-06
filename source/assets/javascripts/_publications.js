326fba7b4266d44338acf05e3d1517ce036f45d3
Murtaza Gulamali <mygulamali@gmail.com>
2014-10-06 17:30:48 +0100

    Concatenate JS into one file (for now).

--- source/assets/javascripts/_publications.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('.abstract-toggle').on('click', function (e) {
-    $(this).parent().find('.abstract-content').slideToggle('fast');
-    $(this).find('i').toggleClass('icon-chevron-right icon-chevron-down');
-    var action = $(this).find('span');
-    action.text() === 'Show' ? action.text('Hide') : action.text('Show');
-  });
-});
--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,8 +1,18 @@
 //= require jquery
-//= require_tree .
 
 $(document).ready(function () {
+  // make footer sticky if window is too tall for content
   if ($('body').height() < $(window).height()) {
     $('footer').css('position', 'absolute').css('bottom', '0');
+  }
+
+  // enable publication abstracts to be toggled on large screens
+  if ($('body.publications_index')) {
+    $('.abstract-toggle').on('click', function (e) {
+      $(this).parent().find('.abstract-content').slideToggle('fast');
+      $(this).find('i').toggleClass('icon-chevron-right icon-chevron-down');
+      var action = $(this).find('span');
+      action.text() === 'Show' ? action.text('Hide') : action.text('Show');
+    });
   }
 });
