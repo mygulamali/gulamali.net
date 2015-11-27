3813583a09cd9f27c7a671b9b33a0c419ac89bb1
Murtaza Gulamali <mygulamali@gmail.com>
2015-11-27 15:56:45 +0000

    Remove commented out code

--- config.rb
+++ config.rb
@@ -1,48 +1,9 @@
 @site_title = 'Murtaza Gulamali'
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
 # configure :development do
 #   activate :livereload
 # end
 
-# Methods defined in the helpers block are available in templates
 helpers do
   def home_page_link
     (current_page.url == '/') ? @site_title : link_to(@site_title, '/')
@@ -72,15 +33,8 @@ activate :deploy do |deploy|
   deploy.build_before = true
 end
 
-# Build-specific configuration
 configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
