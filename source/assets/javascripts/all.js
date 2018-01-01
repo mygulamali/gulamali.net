07a9b331bffa1f2b1a7d632439128870fb62ecdb
Murtaza Gulamali <mygulamali@gmail.com>
2018-01-01 17:10:15 +0000

    Use Font Awesome icon fonts

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
   if ($('body.publications_index')) {
     $('.abstract-toggle').on('click', function (e) {
       $(this).parent().find('.abstract-content').toggle();
-      $(this).find('i').toggleClass('icon-circle-right icon-circle-down');
+      $(this).find('i').toggleClass('icon-chevron-up icon-chevron-down');
       var action = $(this).find('span');
       action.text() === 'Show' ? action.text('Hide') : action.text('Show');
     });
