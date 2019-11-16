f4407b20bf1a775711455b89cb0dbdfab9930e1d
Murtaza Gulamali <murtaza@gulamali.net>
2019-11-16 15:55:37 +0000

    Restyle publications page using Bulma

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -3,12 +3,13 @@ require('../stylesheets/all.scss');
 const $ = require('jquery');
 
 $(document).ready(function () {
-  if ($('body.publications_index')) {
-    $('.abstract-toggle').on('click', function (e) {
-      $(this).parent().find('.abstract-content').toggle();
-      $(this).find('i').toggleClass('icon-chevron-up icon-chevron-down');
-      var action = $(this).find('span');
-      action.text() === 'Show' ? action.text('Hide') : action.text('Show');
-    });
+  if ($('body.publications')) {
+    function toggleAbstract(event) {
+      const $button = $(event.currentTarget);
+      $button.next('.abstract').toggle();
+      $button.find('i').toggleClass('icon-chevron-up icon-chevron-down');
+    }
+
+    $('button').on('click', toggleAbstract);
   }
 });
