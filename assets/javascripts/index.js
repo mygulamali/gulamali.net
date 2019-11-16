18bd2c33c6c30967360c3f3cfc33797b6448e043
Murtaza Gulamali <murtaza@gulamali.net>
2019-11-16 13:18:53 +0000

    Restyle homepage using Bulma

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -2,22 +2,7 @@ require('../stylesheets/all.scss');
 
 const $ = require('jquery');
 
-// make footer sticky if window is too tall for content
-function stickFooterAtBottom() {
-  var $footer = $('footer');
-
-  if ($('body').height() < $(window).height()) {
-    $footer.css('position', 'absolute').css('bottom', '0');
-  } else {
-    $footer.css('position', 'relative').css('bottom', '');
-  }
-}
-
 $(document).ready(function () {
-  stickFooterAtBottom();
-  $(window).on('resize', stickFooterAtBottom);
-
-  // enable publication abstracts to be toggled on large screens
   if ($('body.publications_index')) {
     $('.abstract-toggle').on('click', function (e) {
       $(this).parent().find('.abstract-content').toggle();
