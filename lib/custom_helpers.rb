a5a567a0a2ff9467e0445280074a82722be37067
Murtaza Gulamali <murtaza@gulamali.net>
2019-11-17 19:02:39 +0000

    Restyle page header and footer

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -7,8 +7,4 @@ module CustomHelpers
     return site_title unless current_page.data.title
     "#{current_page.data.title} < #{site_title}"
   end
-
-  def home_page_link
-    (current_page.url == "/") ? site_title : link_to(site_title, "/")
-  end
 end
