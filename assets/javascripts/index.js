2826c0c855e0dda48b97da7117d20f92d8685dd8
Murtaza Gulamali <murtaza@gulamali.net>
2019-11-20 17:52:59 +0000

    Remove unrequired JavaScript for publications page

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,15 +1,1 @@
 require('../stylesheets/all.scss');
-
-function ready(fn) {
-  if (document.readyState != 'loading') {
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-};
-
-ready(function () {
-  if (document.body.classList.contains('publications')) {
-    require('./publications');
-  }
-});
