390466aebd58c0532a8df6bedae0bbceecac5061
Murtaza Gulamali <murtaza@gulamali.net>
2021-12-23 23:55:14 +0000

    Static files are copied to output directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("source/assets/fonts");
+  eleventyConfig.addPassthroughCopy("source/favicon.ico");
+  eleventyConfig.addPassthroughCopy("source/mygulamali.asc");
+  eleventyConfig.addPassthroughCopy("source/robots.txt");
 
   return {
     dir: {
