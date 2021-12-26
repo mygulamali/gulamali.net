2a4c95db8f8b8ba9d9b3dd231fba6f06fa815268
Murtaza Gulamali <murtaza@gulamali.net>
2021-12-26 14:14:11 +0000

    Add pageTitle shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
+const pageTitle = title => {
+  siteTitle = process.env.SITE_TITLE;
+  if (title) {
+    return `${title} < ${siteTitle}`;
+  }
+
+  return siteTitle;
+};
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("source/assets/fonts");
   eleventyConfig.addPassthroughCopy("source/favicon.ico");
   eleventyConfig.addPassthroughCopy("source/mygulamali.asc");
   eleventyConfig.addPassthroughCopy("source/robots.txt");
   eleventyConfig.addWatchTarget("source/assets/stylesheets/");
+  eleventyConfig.addShortcode("pageTitle", pageTitle);
 
   return {
     dir: {
